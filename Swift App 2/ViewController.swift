2cbdaa2b905cedf68ef311b45268ca7204a7aa68
Spencer Johnson <spencer.johnson10@gmail.com>
2016-09-20 21:09:29 -0400

    First Commit

--- Swift App 2/ViewController.swift
+++ Swift App 2/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func tapButton(_ sender: AnyObject) {
+        
+        theLabel.text = "The answer is \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+
+    
+    
+    
 
 }
 
